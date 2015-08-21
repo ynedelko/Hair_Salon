b7537b23688c69e866c130c7776818b467d59537
Yelena Nedelko <ynedelko@gmail.com>
2015-08-21 10:46:56 -0700

    Add passing specs and code to start the array with no stylists and return the name of the stylists.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,1 +1,17 @@
 require('spec_helper')
+
+describe(Stylist) do
+  describe(".all") do
+    it("starts of with no stylists") do
+    expect(Stylist.all()).to(eq([]))
+  end
+end
+
+  describe("#name") do
+    it("it tells you the name of the stylist") do
+      stylist = Stylist.new({:name => "Lulu", :id => nil})
+      expect(stylist.name()).to(eq("Lulu"))
+    end
+  end
+
+end
