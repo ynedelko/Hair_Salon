0edbc600234e6517c2468a7eb562199586ef999e
Yelena Nedelko <ynedelko@gmail.com>
2015-08-21 13:23:34 -0700

    Add integration_spec file.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,3 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 require('spec_helper')
+
+describe('adding a new stylist', {:type => :feature}) do
+  it('allows a user to view a list of stylists') do
+    visit('/')
+    click_link('View Stylist')
+    expect(page).to have_content('Stylists')
+  end
+end
