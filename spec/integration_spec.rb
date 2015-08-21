c2a0ff9c7924748ab8ee41fb4d1dc4e388335ba8
Yelena Nedelko <ynedelko@gmail.com>
2015-08-21 16:37:56 -0700

    Allow user to see list of all clients.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,9 +5,17 @@ set(:show_exceptions, false)
 require('spec_helper')
 
 describe('viewing all the stylists', {:type => :feature}) do
-  it('allows a user to see all of the list that have been created') do
+  it('allows a user to see all of the stylists that have been added') do
     visit('/')
     click_link('View All Stylists')
     expect(page).to have_content('stylists')
+  end
+end
+
+describe('viewing all the clients', {:type => :feature}) do
+  it('allows a user to see all of the clients that have been created') do
+    visit('/')
+    click_link('View All Clients')
+    expect(page).to have_content('clients')
   end
 end
