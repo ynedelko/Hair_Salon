c930aa1efd859bbdd8ba0b11831b7368d183d76b
Yelena Nedelko <ynedelko@gmail.com>
2015-08-21 11:20:08 -0700

    Add passing specs and code to save a stylist, set the stylist ID when you save them, return the stylist by ID, and treat stylist with the same name as the same stylist.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -16,4 +16,24 @@ class Stylist
     end
     stylists
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_stylist|
+    self.id().==(another_stylist.id())
+  end
+
+  define_singleton_method(:find) do |id|
+    found_stylist = nil
+    Stylist.all().each() do |stylist|
+      if stylist.id().==(id)
+        found_stylist = stylist
+      end
+    end
+    found_stylist
+  end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -14,4 +14,39 @@ end
     end
   end
 
+  describe("#id") do
+    it("sets the stylist ID when you save it") do
+      stylist = Stylist.new({:name => "Lulu", :id =>nil})
+      stylist.save()
+      expect(stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe("#save") do
+    it("lets you save the stylists to the database") do
+      stylist = Stylist.new({:name => "Larry", :id => nil})
+      stylist.save()
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same stylist if they have the same name") do
+      stylist1 = Stylist.new({:name => "Lou", :id => nil})
+      stylist2 = Stylist.new({:name => "Lou", :id => nil})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+
+  describe(".find") do
+    it("returns a stulist by their ID") do
+      test_stylist1 = Stylist.new({:name => "Sam", :id => nil})
+      test_stylist1.save()
+      test_stylist2 = Stylist.new({:name => "Sue", :id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+    end
+  end
+
+
 end
